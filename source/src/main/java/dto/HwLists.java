ea47cc8fdeb0e8162373fd1a8e56ac858cbf53fb
plusdojo2026-matsumoto <matsumoto-kosuke-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 15:45:35 +0900

    HwLists

--- source/src/main/java/dto/HwLists.java
+++ source/src/main/java/dto/HwLists.java
@@ -1,10 +1,91 @@
 package dto;
+import java.io.Serializable;
 
-public class HwLists {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+public class HwLists implements Serializable{
+	private int id;
+	private String date;
+	private String range;
+	private int grade;
+	private int class_number;
+	private int homework_id;
+	private String created_at;
+	private String updated_at;
+	
+//	ゲッタとセッタ
+	
+	public int getId() {
+		return id;
 	}
-
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getRange() {
+		return range;
+	}
+	public void setRange(String range) {
+		this.range = range;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public int getClass_number() {
+		return class_number;
+	}
+	public void setClass_number(int class_number) {
+		this.class_number = class_number;
+	}
+	public int getHomework_id() {
+		return homework_id;
+	}
+	public void setHomework_id(int homework_id) {
+		this.homework_id = homework_id;
+	}
+	public String getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+	public String getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(String updated_at) {
+		this.updated_at = updated_at;
+	}
+	
+	public HwLists(int id, String date, String range, int grade, int class_number, int homework_id, String created_at,
+			String updated_at) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.range = range;
+		this.grade = grade;
+		this.class_number = class_number;
+		this.homework_id = homework_id;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+	}
+	public HwLists() {
+		super();
+		this.id = 0;
+		this.date = "";
+		this.range = "";
+		this.grade = 0;
+		this.class_number = 0;
+		this.homework_id = 0;
+		this.created_at = "";
+		this.updated_at = "";
+	}
 }
+	
+
+	
