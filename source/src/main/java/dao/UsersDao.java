f491e3e538a8c176ff130001d642fda45ab111b6
plusdojo2026-kawaguchi <kawaguchi-shun-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 14:29:53 +0900

    UsersDAO編集

--- source/src/main/java/dao/UsersDao.java
+++ source/src/main/java/dao/UsersDao.java
@@ -1,10 +1,61 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dto.Users;
+
 public class UsersDao {
+	// 引数で指定されたidpwでログイン成功ならtrueを返す
+	public boolean isLoginOK(Users user) {
+		Connection conn = null;
+		boolean loginResult = false;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
 
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/B5?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"root", "password");
+
+			// SELECT文を準備する
+			String sql = "SELECT count(*) FROM USERS WHERE login_id=? AND login_pass=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, user.getLogin_id());
+			pStmt.setString(2, user.getLogin_pass());
+
+			// SELECT文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// ユーザーIDとパスワードが一致するユーザーがいれば結果をtrueにする
+			rs.next();
+			if (rs.getInt("count(*)") == 1) {
+				loginResult = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			loginResult = false;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			loginResult = false;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					loginResult = false;
+				}
+			}
+		}
+
+		// 結果を返す
+		return loginResult;
 	}
-
 }
