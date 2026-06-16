90ea8fa1cc85549fa459fbdefc093bd6da0c9bed
plusdojo2026-yamada-mi <yamada-misako-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 17:37:36 +0900

    0616山田

--- source/src/main/webapp/js/student_info.js
+++ source/src/main/webapp/js/student_info.js
@@ -1,3 +1,11 @@
-/**
- * 
- */
+'use strict';
+
+//日付の表示(js)
+
+const now = new Date();
+
+const month = now.getMonth() + 1;
+const day = now.getDate();
+
+document.getElementById('today').textContent =
+    `${month}月${day}日`;
