b454e5f95a40250b5cffb2507de5920019720901
plusdojo2026-kawaguchi <kawaguchi-shun-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 11:53:02 +0900

    ItemsDao編集

--- source/src/main/java/dao/ItemsDao.java
+++ source/src/main/java/dao/ItemsDao.java
@@ -1,5 +1,58 @@
 package dao;
-//岩本//
-public class ItemsDao {
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dto.Items;
+
+public class ItemsDao {
+	// 引数で指定されたIDに対応するitemオブジェクト(持ち物名)を返す
+	public Items getItemByID(int id) {
+		Connection conn = null;
+		Items item = null;
+		
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/B5?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"root", "password");
+			
+			// SELECT文を準備する
+			String sql = "SELECT * FROM ITEMS WHERE id=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			//引数のidを上記のSELECT文の?に代入
+			pStmt.setString(1, String.valueOf(id));
+
+			// SELECT文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+			
+			// IDが一致するリアクションの名前と画像URLをオブジェクトに詰める
+			if(rs.next()) {
+				item = new Items();
+				item.setId(rs.getInt("id"));
+				item.setItem_name(rs.getString("item_name"));
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// reactionを返す
+		return item;
+	}
 }
