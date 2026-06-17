caac80e696340353926eca2a7038996c78fedc49
plusdojo2026-kawaguchi <kawaguchi-shun-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 10:17:03 +0900

    ReactionsDao編集

--- source/src/main/java/dao/ReactionsDao.java
+++ source/src/main/java/dao/ReactionsDao.java
@@ -9,30 +9,32 @@ import java.sql.SQLException;
 import dto.Reactions;
 
 public class ReactionsDao {
-	// 引数で指定されたstampIDに対応する~オブジェクトを返す
-	public Reactions getTeacherByUserID(Int id) {
+	// 引数で指定されたIDに対応するreactionオブジェクトを返す
+	public Reactions getReactionByID(int id) {
 		Connection conn = null;
 		Reactions reaction = null;
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("com.mysql.cj.jdbc.Driver");
+			
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/B5?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 					"root", "password");
+			
 			// SELECT文を準備する
 			String sql = "SELECT * FROM REACTIONS WHERE id=?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			//引数のidを上記のSELECT文の?に代入
-			pStmt.setString(1, id);
+			pStmt.setString(1, String.valueOf(id));
 
 			// SELECT文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
-			// IDが一致するユーザーがいればオブジェクトに詰める（rs.next() == true → 一致するものがあったということ）
+			// IDが一致するリアクションの名前と画像URLをオブジェクトに詰める（rs.next() == true → 一致するものがあったということ）
 			if(rs.next()) {
 				reaction = new Reactions();
 				reaction.setId(rs.getInt("id"));
-				reaction.setReaction_name(rs.getInt("reaction_name"));
+				reaction.setReaction_name(rs.getString("reaction_name"));
 				reaction.setReaction_url(rs.getString("reaction_url"));
 			}
 			
@@ -48,9 +50,7 @@ public class ReactionsDao {
 				}
 			}
 		}
-			// teacherを返す
+			// reactionを返す
 		return reaction;
 	}
-	
-	
 }
