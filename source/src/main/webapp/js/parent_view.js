efc1242592bc0507a99b3573cc9931b2d3311ea7
plusdojo2026-kawaguchi <kawaguchi-shun-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 10:35:07 +0900

    parent_notice,viewのjs編集

--- source/src/main/webapp/js/parent_view.js
+++ source/src/main/webapp/js/parent_view.js
@@ -1,3 +1,10 @@
-/**
- * 
- */
+//日付の表示(js)
+
+const now = new Date();
+
+const month = now.getMonth() + 1;
+const day = now.getDate();
+const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
+
+document.getElementById('today').textContent =
+    `${month}月${day}日(${weekdays[now.getDay()]})`;
