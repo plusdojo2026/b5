dc1f15913a2530a7eaac8f2cc85a9b3596bda61f
plusdojo2026-suto <suto-daichi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 10:31:51 +0900

    StampServlet、StampLogDao(選んだスタンプを選んだ児童に付与する機能)作成

--- source/src/main/java/servlet/StampServlet.java
+++ source/src/main/java/servlet/StampServlet.java
@@ -1,10 +1,40 @@
 package servlet;
 
-public class StampServlet {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import dao.StampLogDao;
+
+@WebServlet("/StampServlet")
+public class StampServlet extends HttpServlet{
+private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// スタンプページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/stamp.jsp");
+		dispatcher.forward(request, response);
 	}
-
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		//取得したいデータ（スタンプの種類、誰に付与するか）
+		String stampId = request.getParameter("stamp_id");
+		String[] selectedStudents = request.getParameterValues("student");//student_idを取得したい
+		
+		StampLogDao stampLogDao = new StampLogDao();
+		
+		for(String s:selectedStudents) {
+			stampLogDao.addStampLog(Integer.parseInt(s),Integer.parseInt(stampId));
+		}
+	}
+	
 }
