9b1e09f3600ede243cfdab4f6b6d67dc58ff6d56
plusdojo2026-yamada-mi <yamada-misako-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 17:24:28 +0900

    StudentInfoServlet(totyuu)

--- source/src/main/java/servlet/StudentInfoServlet.java
+++ source/src/main/java/servlet/StudentInfoServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -10,7 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.ReactionsDao;
 import dao.StampLogDao;
+import dto.Reactions;
 import dto.StampLog;
 
 @WebServlet("/StudentInfoServlet")
@@ -40,7 +43,30 @@ private static final long serialVersionUID = 1L;
 	    // 検索結果をリクエストスコープに格納する
 		request.setAttribute("stampLog", stamplog);
 		
+		//選択した児童の保護者のリアクションを確認する
+		//リアクションを取得
+		
+		ReactionsDao reactionDao = new ReactionsDao();
+
+		List<Reactions> reactionList = new ArrayList<>();
+
+		for (StampLog log : stamplog) {
+
+		    int reaction_id = log.getReaction_id();
+
+		    if (reaction_id != 0) {
+
+		        Reactions reaction =
+		                reactionDao.getReactionByID(reaction_id);
+
+		        reactionList.add(reaction);
+		    }
+		}
+		        
+		    request.setAttribute("reactionList", reactionList);       
+		
 		// 保護者の通知画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/student_info.jsp");
 		dispatcher.forward(request, response);}
+
 }
