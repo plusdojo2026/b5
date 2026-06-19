4e061c1cb3bea2491751b3218d6c50b768837673
plusdojo2026-suto <suto-daichi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 14:54:09 +0900

    StudentsDaoにgetClassMenberメソッドを追加

--- source/src/main/java/dao/StudentsDao.java
+++ source/src/main/java/dao/StudentsDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import dto.Students;
 
@@ -21,7 +23,7 @@ public class StudentsDao {
 						+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 						"root", "password");
 				// SELECT文を準備する
-				String sql = "SELECT * FROM studentS WHERE user_id=?";
+				String sql = "SELECT * FROM students WHERE user_id=?";
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 				//引数のuserIDを上記のSELECT文の?に代入
 				pStmt.setString(1, String.valueOf(userId));
@@ -51,5 +53,51 @@ public class StudentsDao {
 			}
 				// studentを返す
 			return student;
+		}
+		
+		public List<Students> getClassMenber(int grade,int class_number) {
+			Connection conn = null;
+			List<Students> st = new ArrayList<Students>();
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/B5?"
+						+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+						"root", "password");
+				// SELECT文を準備する
+				String sql = "SELECT * FROM students WHERE grade=?,class_number";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+				//引数のuserIDを上記のSELECT文の?に代入
+				pStmt.setInt(1, grade);
+				pStmt.setInt(2, class_number);
+
+				// SELECT文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+				// IDが一致するユーザーがいればオブジェクトに詰める（rs.next() == true → 一致するものがあったということ）
+				if(rs.next()) {
+					Students  student = new Students();
+					student.setId(rs.getInt("id"));
+					student.setUser_id(rs.getInt("user_id"));
+					student.setName(rs.getString("name"));
+					student.setGrade(rs.getInt("grade"));
+					student.setClass_number(rs.getInt("class_number"));
+					
+					st.add(student);
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+				// studentを返す
+			return st;
 		}
 }
