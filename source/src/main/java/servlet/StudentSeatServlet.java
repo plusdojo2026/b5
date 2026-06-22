58d374fa6056ecbe0c39301a428806822ae3c9e4
plusdojo2026-suto <suto-daichi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 11:01:46 +0900

    StudentSeatServletのPost作成

--- source/src/main/java/servlet/StudentSeatServlet.java
+++ source/src/main/java/servlet/StudentSeatServlet.java
@@ -11,10 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.StampLogDao;
 import dao.StudentsDao;
 import dto.Students;
 import dto.Teachers;
-
 @WebServlet("/StudentSeatServlet")
 public class StudentSeatServlet extends HttpServlet{
 private static final long serialVersionUID = 1L;
@@ -38,11 +38,10 @@ private static final long serialVersionUID = 1L;
 		dispatcher.forward(request, response);
 		*/
 		
+		//テスト用、1年1組で固定
 		//セッションからログイン中の教師データを取得する→学年、クラスをintにする
-		HttpSession session = request.getSession();
-		Teachers teacher = (Teachers) session.getAttribute("teacherData");
-		int grade = teacher.getGrade();
-		int class_number = teacher.getClass_number();
+		int grade = 1;
+		int class_number = 1;
 		//クラスと学年からクラスの児童一覧を取得
 		StudentsDao sDao = new StudentsDao();
 		List<Students> st = sDao.getClassMember(grade,class_number);
@@ -55,15 +54,30 @@ private static final long serialVersionUID = 1L;
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		Teachers teacher = (Teachers) session.getAttribute("teacherData");
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		
-		//取得したいデータ（スタンプの種類）
-		String stampId = request.getParameter("stamp_id");
+		//取得したいデータ（スタンプの種類,選択した児童のリスト）
+		int stamp_id = (int) session.getAttribute("stamp_id");
+		String[] selectedStudents = request.getParameterValues("student");//student_idを取得したい
 		
-		//セッションスコープにstamp_idを格納
-		HttpSession session = request.getSession();
-		session.setAttribute("stamp_id",stampId);
+		StampLogDao stampLogDao = new StampLogDao();
+		//その他を選んだ時の処理（テキストも挿入する）
+		if(stamp_id== 5) {
+			String text = (String) session.getAttribute("text");
+			for(String s:selectedStudents) {
+				stampLogDao.addStampLog(Integer.parseInt(s),stamp_id,text);
+			}
+		}
+		//その対外を選んだ時の処理（スタンプIDとStudentIDを挿入）
+		else {
+			for(String s:selectedStudents) {
+				stampLogDao.addStampLog(Integer.parseInt(s),stamp_id);
+			}
+		}
 
 	}
 }
