e44e6583d43be407dec9abfe80e65231870e50bc
plusdojo2026-suto <suto-daichi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 13:29:04 +0900

    StudentResultServletのGetを作成

--- source/src/main/java/servlet/StudentResultServlet.java
+++ source/src/main/java/servlet/StudentResultServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,21 +9,44 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.StampLogDao;
+import dto.Students;
 
 @WebServlet("/StudentResultServlet")
 public class StudentResultServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		//ログインしている児童データを取得
+		HttpSession session = request.getSession();
+		Students student = (Students) session.getAttribute("studentData");
+		
+		//必要な変数を取得
+		int student_id = student.getId();
+		int grade = student.getGrade();
+		int class_number = student.getClass_number();
+		// 現在の日付を取得
+		LocalDate now = LocalDate.now();
+		// 現在の月を整数値で取得
+		int month = now.getMonthValue();
 		//student_idから、その児童が各スタンプを何回押されたかを取得する
 		//その回数に応じてスタンプ画像の大きさを変える
+		StampLogDao logDao = new StampLogDao();
+		int[] stampCount = new int[5];
+		
+		for(int i = 0; i< stampCount.length; i++){
+			stampCount[i] = logDao.getStampCount(student_id, i, month);
+		}
+		//スタンプ数をリクエストスコープに格納
+		request.setAttribute("stampCount",stampCount);
 		
 		//クラスのスタンプ数を集計、それに応じて花に使う画像を取得、表示
 		//スタンプ数を集計するために、学年、クラスを引数としてクラスの全員のスタンプログを取得できるメソッドが必要（getTotalClassStamp）
-		//そのログのリストでfor文を回して、その他以外のスタンプ数を集計する
 		//その数に応じて画像を変更する
-		
-		
+		int classStampCount = logDao.getClassStampCount(grade, class_number, month);
+		request.setAttribute("classStampCount",classStampCount);
 		// ログインページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/student_result.jsp");
 		dispatcher.forward(request, response);
